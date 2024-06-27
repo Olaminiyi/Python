b20db0e1df859df6ac9bd2fa9da819ff111986cb
olaminiyi <olaiyaolaminiyi@gmail.com>
2024-06-27 02:10:19 +0100

    added test 4

--- app1/main.py
+++ app1/main.py
@@ -15,3 +15,30 @@
 # for index, file in enumerate(filenames):
 #     print(f"{index}-{file}.txt")
 # =============================================================================
+
+# Test 4
+
+# We have a list of two IPs in the code area.
+
+# Extend the code so your program:
+
+# 1. Prompts the user to input an index (e.g., 0 or 1).
+
+# 2. Depending on the user input, the program should return either You chose 100.122.133.111 or You chose 100.122.133.111
+
+# Note: In order for the system to mark your exercise as correct, your code should return the exact output (i.e., upper case Y in You chose and no spaces or other characters after the IP.
+
+# For example: Enter the index of the IP you want: 1
+# You chose 100.122.133.111 
+
+# Solution
+
+# ips = ['100.122.133.105', '100.122.133.111']
+
+
+# user_prompt = int(input("input an index 0 or 1: "))
+# choice = ips[user_prompt]
+# xchoice = "You chose"
+# print(f'{xchoice} {choice}')
+
+# =========================================================================================
