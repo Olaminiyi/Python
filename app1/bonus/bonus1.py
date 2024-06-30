454ec9d9f366b3272622e4a42e026fd928b0271c
olaminiyi <olaiyaolaminiyi@gmail.com>
2024-06-30 16:29:20 +0100

    testing

---
+++ app1/bonus/bonus1.py
@@ -0,0 +1,8 @@
+contents = ["gbemi soke", "Gbemi dide", "fami lowo soke"]
+
+filenames = ["doc.txt", "report.txt", "presentation.txt"]
+
+for content, filename in zip(contents, filenames):
+    file = open(f"../files/{filename}", 'w')
+    file.write(content)
+
